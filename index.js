3bc2500b614a4612a407e267cc1b19be61c43435
uisawa <6nine11@gmail.com>
2026-08-21 05:13:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 var a = new (require("node-telegram-bot-api").TelegramBot)(process.env.a, {polling: true})
-a.on("message", b => a.sendMessage(b.chat.id, JSON.stringify(b)))
+a.on("message", b => a.sendMessage(b.chat.id, "OK"))
